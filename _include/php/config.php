f44aae09df901e87f3f6f9a2c1af560acc992562
Acera Dryd <brian.meiszner@gmail.com>
2018-02-16 21:18:52 +0100

    -

--- _include/php/config.php
+++ _include/php/config.php
@@ -1,8 +1,8 @@
 <?php
 //URL: uni.dpdgaming.de
 //PHPMyAdming: uni.dpdgaming.de/phpmyadmin
-$db_user = 'rohland'; // Username
-$db_pass = 'Webprogrammierung'; // Password
+$db_user = 'root'; // Username
+$db_pass = ''; // Password
 
 $dbh = new PDO('mysql:host=localhost;dbname=WebProg;charset=UTF8', $db_user, $db_pass);
 ?>
